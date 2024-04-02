c640229ce5b8bd9d92c2afa5382acd47a5856793
Juan Gallego IV <juan@gallegoiv.com>
2024-04-02 23:23:18 +0200

    Format file on save using LSP

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -5,13 +5,22 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.stylua.with({
+          extra_args = { "--indent-type='Spaces' --indent-width=2" },
+        }),
       },
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      pattern = { "*" },
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
   end,
 }
